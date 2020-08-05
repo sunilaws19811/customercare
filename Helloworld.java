8c55fe221a0c9e1322b3fcfdf2a5940742b7cea0
sunilaws19811 <sunil.aws19811@gmail.com>
2020-08-05 18:34:05 +0000

    My second commit

--- Helloworld.java
+++ Helloworld.java
@@ -4,5 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("GIT learning"); 
+        System.out.println("GIT learning Intellipaat");
     }
 }
